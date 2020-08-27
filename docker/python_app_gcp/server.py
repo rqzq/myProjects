a8da2120d39893ea009483cd111b3d070afafab7
rqzq <bishwajeettudaysamanta@gmail.com>
2020-08-27 09:10:34 +0530

    Fixed Indentation

--- docker/python_app_gcp/server.py
+++ docker/python_app_gcp/server.py
@@ -2,17 +2,19 @@ import tornado.ioloop
 import tornado.web
 
 
-class Mainhandler(tornado.web.RequestHandler):
+class MainHandler(tornado.web.RequestHandler):
     def get(self):
-      self.write("Hello World \n")
-      print(self.request)
+        self.write("Hello, world\n")
+        print(self.request)
 
-    def make_app():
-      return tornado.web.Application([
-        (r"/",Mainhandler),
-      ])
 
-if __name__=="__main__":
-  app = make_app()
-  app.listen(8888)
-  tornado.ioloop.IOLoop.current().start()
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
