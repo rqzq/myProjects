e93f9b3d0eb7d8d5acc282542ea4959d5664ea61
rqzq <bishwajeettudaysamanta@gmail.com>
2020-09-09 15:55:05 +0530

    Terraform Code for GCP Instances

---
+++ terraform/Bosh_GCP/README.md
@@ -0,0 +1,7 @@
+## Steps to Create Terraform Infrastructure
+
+* Create Service Account from IAM Dropdown
+    * Assign the Role as Project Editor.
+* Create Key Pair as Json.
+    * Import the Contents in your Terraform Code.
+        
--- terraform/Bosh_GCP/create_instance.tf
+++ terraform/Bosh_GCP/create_instance.tf
@@ -38,5 +38,5 @@ resource "google_compute_firewall" "http-server" {
 }
 
 output "ip" {
-  value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
+  value = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
 }
