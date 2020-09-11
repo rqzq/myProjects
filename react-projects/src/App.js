517417c108cc57976122d925658c4370751dd961
rqzq <bishwajeettudaysamanta@gmail.com>
2020-09-11 18:17:39 +0530

    Using Subscribe Button and with onClick Event generating a message for clicking the button subscribe

--- react-projects/src/App.js
+++ react-projects/src/App.js
@@ -3,28 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
+import Message from "./components/Message";
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Payel" surname="Samanta">
-        <p>
-          This is my Wife Name
-        </p>
-      </Greet>
-      <Greet name="Shinjaann" surname="Samanta">
-        <button>
-          Kiss Me
-        </button>
-      </Greet>
-    <Greet name="Random" surname="Tandem"/>
-    <Welcome name="Random" surname="Tandem" />
-    <Welcome name="Random" surname="Tandom" >
-      <button>
-        Hit Me
-      </button>
-    </Welcome>
-    <Welcome name="Random" surname="Tandum" />
+      <Message />
     </div>
   );
 }
