59a336f7c4c65d25c87d8f705fc5b9ba30652589
rqzq <bishwajeettudaysamanta@gmail.com>
2020-09-22 08:47:35 +0530

    Updated Notes about Function Call and Function

--- react-projects/src/components/FunctionClick.js
+++ react-projects/src/components/FunctionClick.js
@@ -2,6 +2,8 @@ import React from "react";
 
 // ClickHandler is not calling the function, whereas clickHandler() is calling the function.
 // so we dont need to use clickHandler()
+// Event Handler is a Function not a Function Call.
+// If we use Function call, event will be triggered automatically however if we click no response would be there.
 
 function FunctionClick() {
     function clickHandler() {
